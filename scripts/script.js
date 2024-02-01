8a32d45c37d4a7f128f5e8b71a4aa8fb083a7c5f
mmandelyn <157517770+mmandelyn@users.noreply.github.com>
2024-02-01 11:30:38 -0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+// Mandy McGinnis
+
+$(document).ready(function(){
+  
+  $("img").draggable(); 
+  
+  //Move the image to the finish line
+  console.log("Top: " + $("img").position().top + " Left: " + $("img").position().left);
+  $("img").draggable({
+    stop: function(){
+      console.log("stopped");
+
+      let dogeLeft = $("img").position().left;
+
+      if (dogeLeft >= 1000){
+        console.log("above 1000");
+        $("#updates").text("You made it!");
+      }
+      else{
+        console.log("under 1000");
+        $("#updates").text("Almost there..");
+      }
+    } 
+  }) 
+  
+  //Double click to reload the page
+  $("img").dblclick(function(){
+    location.reload();
+  })
+});
